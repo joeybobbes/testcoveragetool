f6954835ad5b3835841f5f44d631ee0495b2f6da
joeybobbes <joelkroepelin@googlemail.com>
2021-10-08 22:48:13 +0200

    added modify function, is_mcdc function, adapted foo and tc_mcdc

--- mcdc.py
+++ mcdc.py
@@ -1,4 +1,5 @@
-""" calculate number of required test cases for different coverage criteria
+""" 
+calculate number of required test cases for different coverage criteria
 1. statement coverage
 2. decision coverage
 3. condition coverage
@@ -9,24 +10,20 @@
 for a program that has n binary input conditions
 """
 
-def foo(B,C):
-    """ example function consists of one if statement A,
-    conditions B,C and decision D 
-    for this program to work we need a friendly function that is able to return the number of statements and decisions that were called 
-    """
+def foo(*conditions):
+    """ example function inputs conditions and returns decision D """
     
-    if B and C:
-        A=True
+    if all(conditions):
         D=True
     else:
-        A=True
         D=False
-    return [A,B,C,D]
+    return D
 
 from inspect import signature
 
 def tc_sc(foo):
-    """ calculates (number of) required test cases for foo
+    """ 
+    calculates (number of) required test cases for foo
     for condition coverage criterion. for that all possible
     test cases (multiple condition coverage) are run and we
     search for the smallest subset that satisfies condition
@@ -54,6 +51,35 @@ def tc_sc(foo):
     satisfies_cc.sort(key=lambda x: x[1])
     return satisfies_cc[0][1]
         
+def tc_mcdc(foo):
+    """ 
+    calculates (number of) required test cases for foo
+    for modified condition decision coverage criterion. for that all possible
+    test cases (multiple condition coverage) are run and we
+    search for the smallest subset that satisfies condition
+    coverage
+
+    >>> tc_mcdc(foo)
+    3
+    """
+
+    # get number of input parameters (=conditions)
+    sig=signature(foo)
+    n=len(sig.parameters)
+    
+    # generate all possible test cases
+    all_test_cases=tc_gen(n)
+
+    # generate all subsets of test cases
+    allsubsets=powerset(all_test_cases)
+
+    # create list with all subsets that satisfy cc and their length
+    satisfies_mcdc=[]
+    for subset in allsubsets:
+        if is_mcdc(subset,foo):
+            satisfies_mcdc.append((subset,len(subset)))
+    satisfies_mcdc.sort(key=lambda x: x[1])
+    return satisfies_mcdc[0][1]
 
 import itertools
 
@@ -115,6 +141,48 @@ def is_cc(test_cases):
         else:
             cc.append(False)      
     return all(cc)
+
+def is_mcdc(test_cases, foo):
+    """ 
+    return true if set of test cases satisfies modified decision condition 
+    coverage criteria 
+
+    >>> is_mcdc(([True,True],[False,True],[True,False]), foo)
+    True
+    >>> is_mcdc(([False,False],[False,True],[True,False]), foo)
+    True
+    """
+    
+    mcdc=[]
+    for test_case in test_cases:
+        mod_test_cases=modify(test_case)
+        changed_decision=[]
+        for mod_test_case in mod_test_cases:
+            changed_decision.append(foo(*test_case)!=foo(*mod_test_case))
+        mcdc.append(any(changed_decision))
+    return all(mcdc)
+
+def modify(test_case):
+    """ return iterable of modifications 
+        [True, False]->[False, False] for i=0
+        [True, False]->[True, True] for i=1
+        
+        we get 2^n modified test case for n conditions
+    
+    >>> modify([True, False])
+    [[False, False], [True, True]]
+    """
+    n=len(test_case)
+    mod_test_cases=[]
+    for i in range(n):
+        mod_test_case=test_case[:]
+        mod_test_case[i]= not mod_test_case[i]
+        mod_test_cases.append(mod_test_case)
+    return mod_test_cases
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
 
 
     
